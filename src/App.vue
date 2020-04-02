ee686428725e4b9b60fdd14fee5fd772f8e2199c
Chris Stroud <chris.stroud@boughtbymany.com>
2020-04-02 10:17:52 +0100

    feat: add patterns

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <template>
+  <div>
+    <card>
+      <h1>Hello</h1>
+    </card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Card } from '@boughtbymany/patterns'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '@boughtbymany/patterns/src/sass/ui/ui.scss';
 </style>
+
